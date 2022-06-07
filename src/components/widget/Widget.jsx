b981b3a7826499a2fa89f8a4d2e6ba9565a7e437
choimin1136 <choimin1136@gmail.com>
2022-06-07 16:10:30 +0900

    useState 변수 처리 commit

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -2,7 +2,7 @@ import "./widget.scss";
 import React, { useContext, useEffect, useState } from 'react';
 import { Button } from "react-bootstrap";
 import { auth, db } from "../../firebase";
-import { collection, query, where, onSnapshot, getDocs, doc, documentId } from "firebase/firestore";
+import { collection, query, where, onSnapshot, getDocs, getDoc, doc, documentId, docs} from "firebase/firestore";
 import { AuthContext } from "../../context/AuthContext";
 
 
@@ -12,7 +12,9 @@ const Widget = () => {
   let [location, location_up] = useState(["첨단 3D 상용화지원센터", "하남 kbi 지식산업센터"])
   let [atten, atten_up] = useState(true)
   let [atten_date, attten_date_up] = useState(["2022.xx.xx", "2022.x1.x1"])
-  let [data, setData] = useState([]);
+  let [empData, setEmp] = useState();
+  let [tagData, setTag] = useState();
+  let [logData, setLog] = useState();
 
   let {log_dispatch} = useContext(AuthContext);
 
@@ -21,40 +23,60 @@ const Widget = () => {
 
   useEffect(() => {
     const userData = JSON.parse(localStorage.getItem("user"));
-    console.log(userData.email);
+    const tag_serial = "testBeacon";
+    // console.log(userData.email);
 
     const getLog = async () => {
-      let list = [];
       try {
-        const querySnapshot = await getDocs(collection(db, "employee"));
-        querySnapshot.forEach((doc) => {
-          if (userData.email == doc.data().email) {
-            list.push({id:doc.id, ...doc.data()});
-          }
-        });
-        const querySnapshot1 = await getDocs(collection(db, "beaconTag"));
-        querySnapshot1.forEach((doc) => {
-          if (list[0].tag_serial == doc.id) {
-            list.push({id:doc.id, ...doc.data()});
-          }
-        });
-        const querySnapshot2 = await getDocs(collection(db, "beaconTag/"+list[1].id+"/log"));
-        const logList = [];
-        querySnapshot2.forEach((doc) => {
-          logList.push({id:doc.id, ...doc.data()});
-        });
-        list.push(logList);
-        setData(list);
+        //로그인 데이터 기반 사원 데이터 호출
+        const querySnapshot = await getDocs(query(collection(db, "employee"), where("email", "==", userData.email)));
+        querySnapshot.docs.map((doc)=>(
+          setEmp({id:doc.id, ...doc.data()})
+        ))
+
+        // querySnapshot.map.docs((doc) => {
+        //   if (userData.email === doc.data().email) {
+        //     setEmp({id:doc.id, ...doc.data()});
+        //   }
+        // })
+
+        //태그 정보 호출
+        const docRef = doc(db, "beaconTag", empData.tag_serial.toString());
+        const docSnap = await getDoc(docRef);
+        setTag({id:docSnap.id, ...docSnap.data()})
+        console.log(tagData.id.toString())
+
+        // querySnapshot1.map.docs((doc) => {
+        //   if (empData.tag_serial === doc.id) {
+        //     setTag({id:doc.id, ...doc.data()});
+        //   }
+        // });
+
+        //로그 정보 호출
+        const querySnapshot2 = await getDocs(query(collection(db, "beaconTag/"+tagData.id.toString()+"/log")));
+        querySnapshot2.docs.map((doc)=>(
+          setLog({id:doc.id, ...doc.data()})
+        ))
+        // const querySnapshot2 = await getDocs(collection(db, "beaconTag/"+tagData.id.toString()+"/log"));
+        // const logList = [];
+        // querySnapshot2.map.docs((doc) => {
+        //   logList.push({id:doc.id, ...doc.data()});
+        // });
+
+        //Location 정보 호출
+        
       } catch (err) {
         console.log(err);
-        alert('로그인 시간 만료');
-        log_dispatch({type: "LOGOUT"});
+        // alert('로그인 시간 만료');
+        // log_dispatch({type: "LOGOUT"});
       }
     };
     getLog();
   }, []);
 
-  console.log(data)
+  console.log(empData)
+  console.log(tagData)
+  console.log(logData)
 
   return (
     <div className="atten">
